7b9b7dd534ad480c6c5be7dfb4884b76494b6db8
Kerby <kn1578@nyu.edu>
2020-11-12 01:04:56 +0800

    Another lighting test (within the scene lighting test)

---
+++ Assets/followPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform target;
+
+
+    private void FixedUpdate() {
+
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -5);
+    }
+}
