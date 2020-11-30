51f9ca7d0887eada80d52828f8f87655fe2d5acc
wuji <wc1629@nyu.edu>
2020-11-30 12:28:10 -0500

    added basic ui

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameMaster : MonoBehaviour
@@ -9,15 +10,23 @@ public class GameMaster : MonoBehaviour
     public Vector2 lastCheckPointPos;
 
     private SimplePhysicsController sc;
+    private PlayerAttack pa;
+    private Health hl;
+    public Image health;
+    public Image energyBar;
+
     public int score = 0;
 
     private void Start() {
         sc = GameObject.FindGameObjectWithTag("Player").GetComponent<SimplePhysicsController>();
-   
+        pa = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttack>();
+        hl = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
     }
 
     private void Update() {
         //newScore = sc.score;
+        health.rectTransform.sizeDelta = new Vector2(hl.Hp * 10f, 10f);
+        energyBar.rectTransform.sizeDelta = new Vector2(50f, (50f/9f)*pa.energy);
     }
 
     private void Awake() {
