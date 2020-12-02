c97fc7073b7ed6b407a83c19037741cf61af4f33
Kerby <kn1578@nyu.edu>
2020-12-02 20:10:21 +0800

    Missing Asset Bug Fixes

---
+++ Assets/effect_hit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effect_hit : MonoBehaviour
+{
+    public Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("end"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
