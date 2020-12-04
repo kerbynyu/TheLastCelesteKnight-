e95bf0a43fcd005d589067c4c76af3792360ac20
Hongke <luohongke2000@gmail.com>
2020-12-05 01:40:41 +0800

    phase4 done

--- Assets/Scripts/lightWallScr.cs
+++ Assets/Scripts/lightWallScr.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class lightWallScr : MonoBehaviour
 {
     private int destroyCounter;
+    public float movingSpd = 0.6f;
+    public bool phase4 = false;
+    private int lastingTime = 200;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +16,15 @@ public class lightWallScr : MonoBehaviour
     void FixedUpdate()
     {
         destroyCounter += 1;
-        transform.position += transform.right * 0.6f;
-        if (destroyCounter > 200)
+        transform.position += transform.right * movingSpd;
+        if (destroyCounter > lastingTime)
         {
             Destroy(gameObject);
+        }
+        if (phase4)
+        {
+            movingSpd = 0.4f;
+            lastingTime = 400;
         }
     }
     // Update is called once per frame
