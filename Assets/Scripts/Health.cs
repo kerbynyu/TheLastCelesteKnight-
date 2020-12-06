fc6f60541b076c226e88269090314fdc24c14577
Kerby <kn1578@nyu.edu>
2020-12-07 04:55:36 +0800

    UI "Hearts" and Energy Sprites

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,12 +3,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class Health : MonoBehaviour
 {
     public float maxHp = 10;
     public float Hp;
     float lHp;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +25,26 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
+
+        if(Hp > maxHp) {
+            Hp = Mathf.Min(maxHp, Hp);
+        }
+        
+
+        for(int i = 0; i < hearts.Length; i++) {
+            if (i < Hp) {
+                hearts[i].sprite = fullHeart; 
+            } else {
+                hearts[i].sprite = emptyHeart;
+            }
 
 
-        lHp = Hp;
-        Hp = Mathf.Min(maxHp, Hp);
+            if (i < maxHp) {
+                hearts[i].enabled = true;
+            } else {
+                hearts[i].enabled = false;
+            }
+        }
     }
 }
